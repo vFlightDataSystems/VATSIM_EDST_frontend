29b2648b513de539d5e31dc51a2430da3696c4fe
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-12-20 10:58:47 +0100

    working on AclTable

--- src/components/edst-windows/acl-components/AclTable.jsx
+++ src/components/edst-windows/acl-components/AclTable.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import '../../../css/windows/acl-styles.css';
 
 export default class AclTable extends React.Component {
 
   render() {
-    return (<div className="edst-window-header">
-      <div className="edst-window-header-bottom-row">
-        Add/Find
-        <div className="input-container">
-          <input/>
-        </div>
+    return (<div className="acl-body">
+      <div className="acl-body-row border">
+        <div className="acl-body-col">
+          TEST
+      </div>
       </div>
     </div>);
   }
