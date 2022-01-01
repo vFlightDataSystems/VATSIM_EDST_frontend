962e03c2771c296f912cd8baa75e1c8664b47762
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-01-01 23:02:24 +0100

    minor fixes and adjustments

--- src/components/edst-windows/plans-display-components/PlansDisplayTable.jsx
+++ src/components/edst-windows/plans-display-components/PlansDisplayTable.jsx
@@ -6,11 +6,11 @@ import '../../../css/windows/plans-display-styles.scss';
 export default class PlansDisplayTable extends React.Component {
 
   render() {
-    const {plan_queue, selected_msg, asel} = this.props
+    const {plan_queue, selected_msg, asel} = this.props;
 
     return (<div className="plans-display-body no-select">
       {Object.entries(plan_queue)?.map(([i, p]) =>
-        <div className="body-row" key={`plans-display-body-${p.cid}`}>
+        <div className="body-row" key={`plans-display-body-${p.cid}-${p.msg}-${i}`}>
           <div className={`body-col plans-display-col-1 green hover ${(selected_msg === i && asel?.cid === p.cid) ? 'selected' : ''}`}
                onMouseDown={(event) => {
                  this.props.aircraftSelect(event, 'plans', p.cid, 'type');
