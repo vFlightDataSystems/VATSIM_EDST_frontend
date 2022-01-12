21b3939de68a93b123e36660258c0ed060fba165
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-01-12 03:22:48 +0100

    apparently things break when SortMenu is a function component

--- src/components/edst-windows/SortMenu.jsx
+++ src/components/edst-windows/SortMenu.jsx
@@ -1,29 +1,41 @@
-import {forwardRef, useEffect, useState} from 'react';
+import React from 'react';
 import '../../css/header-styles.scss';
 import '../../css/windows/options-menu-styles.scss';
 
-export const SortMenu = forwardRef((props, ref) => {
-  const [focused, setFocused] = useState(false);
-  const [sorting, setSorting] = useState(props.sorting);
-  
-  useEffect(() => setSorting(props.sorting), [props.sorting]);
+export default class SortMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: false,
+      sorting: this.props.sorting
+    };
+    this.sortMenuRef = React.createRef();
+  }
 
-    const {pos, ref_id} = props;
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.sorting !== prevProps.sorting) {
+      this.setState({sorting: this.props.sorting});
+    }
+  }
+
+  render() {
+    let {focused, sorting} = this.state;
+    const {pos, ref_id} = this.props;
     const acl = ref_id === 'acl-sort-button';
     const dep = ref_id === 'dep-sort-button';
     const selected = acl ? sorting.acl : sorting.dep;
 
     return (<div
-        onMouseEnter={() => setFocused(true)}
-        onMouseLeave={() => setFocused(false)}
+        onMouseEnter={() => this.setState({focused: true})}
+        onMouseLeave={() => this.setState({focused: false})}
         className="options-menu no-select"
-        ref={ref}
+        ref={this.sortMenuRef}
         id="sort-menu"
         style={{left: pos.x, top: pos.y}}
       >
         <div className={`options-menu-header ${focused ? 'focused' : ''}`}
-             onMouseDown={(event) => props.startDrag(event, ref)}
-             onMouseUp={(event) => props.stopDrag(event)}
+             onMouseDown={(event) => this.props.startDrag(event, this.sortMenuRef)}
+             onMouseUp={(event) => this.props.stopDrag(event)}
         >
           Sort Menu
         </div>
@@ -32,7 +44,7 @@ export const SortMenu = forwardRef((props, ref) => {
             <div className="options-col sort"
                  onMouseDown={() => {
                    sorting.acl.sector = !selected.sector;
-                   setSorting(sorting);
+                   this.setState({sorting: sorting});
                  }}
             >
               <div className={`box ${selected?.sector === true ? 'selected' : ''}`}/>
@@ -43,7 +55,7 @@ export const SortMenu = forwardRef((props, ref) => {
             <div className="options-col sort"
                  onMouseDown={() => {
                    sorting[acl ? 'acl' : 'dep'].name = 'ACID';
-                   setSorting(sorting);
+                   this.setState({sorting: sorting});
                  }}
             >
               <div className={`box diamond ${selected?.name === 'ACID' ? 'selected' : ''}`}/>
@@ -54,7 +66,7 @@ export const SortMenu = forwardRef((props, ref) => {
             <div className="options-col sort"
                  onMouseDown={() => {
                    sorting[acl ? 'acl' : 'dep'].name = 'Destination';
-                   setSorting(sorting);
+                   this.setState({sorting: sorting});
                  }}
             >
               <div className={`box diamond ${selected?.name === 'Destination' ? 'selected' : ''}`}/>
@@ -65,7 +77,7 @@ export const SortMenu = forwardRef((props, ref) => {
             <div className="options-col sort"
                  onMouseDown={() => {
                    sorting[acl ? 'acl' : 'dep'].name = 'Origin';
-                   setSorting(sorting);
+                   this.setState({sorting: sorting});
                  }}
             >
               <div className={`box diamond ${selected?.name === 'Origin' ? 'selected' : ''}`}/>
@@ -76,7 +88,7 @@ export const SortMenu = forwardRef((props, ref) => {
             <div className="options-col sort"
                  onMouseDown={() => {
                    sorting.dep.name = 'P-Time';
-                   setSorting(sorting);
+                   this.setState({sorting: sorting});
                  }}
             >
               <div className={`box diamond ${selected?.name === 'P-Time' ? 'selected' : ''}`}/>
@@ -87,15 +99,15 @@ export const SortMenu = forwardRef((props, ref) => {
             <div className="options-col left">
               <button
                 onMouseDown={() => {
-                  props.setSorting(sorting);
-                  props.closeWindow();
+                  this.props.setSorting(sorting);
+                  this.props.closeWindow();
                 }}
               >
                 OK
               </button>
             </div>
             <div className="options-col right">
-              <button onMouseDown={props.closeWindow}>
+              <button onMouseDown={this.props.closeWindow}>
                 Exit
               </button>
             </div>
@@ -103,4 +115,5 @@ export const SortMenu = forwardRef((props, ref) => {
         </div>
       </div>
     );
-})
+  }
+}
