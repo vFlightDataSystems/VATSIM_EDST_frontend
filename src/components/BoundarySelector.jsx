10d1f418397d341a783a42c3cb47598039af081e
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-01-20 10:30:23 -0700

    Moved buttons down to bottom

--- src/components/BoundarySelector.jsx
+++ src/components/BoundarySelector.jsx
@@ -4,16 +4,17 @@ export function BoundarySelector(props){
     return(
         <div class="boundary-selector">
             <div className="boundary-selector-innner">
-                <button className="close-btn" onClick={() => props.changer(false)}>Close</button>
-                <button className="save-btn" onClick={() => props.updatePolygon()}>Save</button>
+                <hr style={{color: '#000000', padding: 20, border: 'none'}}/>
                 <h1>Pick your sectors</h1>
                 <ul>
-                    {props.boundaries.map((name) => {
+                    {props.boundaries.map((name, index) => {
                         return(
-                            <li><label><input type="checkbox" onChange={() => props.updateSelected(name)} value={name}/> {name}</label></li>
+                            <li key={index}><label><input type="checkbox" onChange={() => props.updateSelected(name)} value={name}/>{name}</label></li>
                         )
                     })}
                 </ul>
+                <button className="close-btn" onClick={() => props.changer(false)}>Close</button>
+                <button className="save-btn" onClick={() => props.updatePolygon()}>Save</button>
                 {props.children}
             </div>
         </div>
