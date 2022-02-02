bb559caa0c0c277f01db4dd78e6c01ab48ee92f4
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-02-02 20:31:31 +0100

    add toast notifications when copying to clipboard

---
+++ src/components/toast/Toast.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect} from "react";
+import '../../css/toast.scss';
+
+export interface ToastProps {
+  id: string;
+  destroy: () => void;
+  title: string;
+  content: string;
+  duration?: number;
+}
+
+const Toast: React.FC<ToastProps> = (props) => {
+  const {destroy, content, title, duration = 0, id} = props;
+
+  useEffect(() => {
+    if (!duration) return;
+
+    const timer = setTimeout(() => {
+      destroy();
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [destroy, duration]);
+
+  return (
+    <div>
+      <div className={"toast-header"} id={id}>
+        <div>{title}</div>
+        <button onClick={destroy}>X</button>
+      </div>
+      <div className={"toast-body"}>{content}</div>
+    </div>
+  );
+};
+
+export default Toast;
