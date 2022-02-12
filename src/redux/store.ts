50d797c3825f20a3f17d97063ea58f4641ac986c
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-02-12 13:43:58 +0100

    @reduxjs/toolkit automatically adds the thunk middleware and depends on redux and redux-thunk, so no need to install them separately

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,16 +4,6 @@ import {sectorReducer} from "./reducers/sectorReducer";
 import {configureStore} from "@reduxjs/toolkit";
 import {entriesReducer} from "./reducers/entriesReducer";
 
-const asyncFunctionMiddleware = (storeAPI: any) => (next: any) => (action: any) => {
-  // If the "action" is actually a function instead...
-  if (typeof action === 'function') {
-    // then call the function and pass `dispatch` and `getState` as arguments
-    return action(storeAPI.dispatch, storeAPI.getState);
-  }
-
-  // Otherwise, it's a normal action - send it onwards
-  return next(action);
-};
 
 const store = configureStore({
   reducer: {
@@ -21,8 +11,7 @@ const store = configureStore({
     dep: depReducer,
     sectorData: sectorReducer,
     entries: entriesReducer
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(asyncFunctionMiddleware)
+  }
 });
 
 export default store;
