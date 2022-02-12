02ea4790ded6ee5c0a191e3949715b332349b333
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-02-12 01:20:57 +0100

    extend actions and reducers sector data and reference fixes is now stored in redux, template for entries is there, async actions now work (more or less, I think)

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -3,10 +3,20 @@ import {
   ADD_DEP_CID,
   DELETE_ACL_CID,
   DELETE_DEP_CID,
-  SET_ACL_CID_LIST, SET_ACL_MANUAL_POSTING,
+  SET_ACL_CID_LIST,
+  SET_ACL_MANUAL_POSTING,
   SET_ACL_SORT,
-  SET_DEP_CID_LIST, SET_DEP_MANUAL_POSTING, SET_DEP_SORT
+  SET_ARTCC_ID,
+  SET_DEP_CID_LIST,
+  SET_DEP_MANUAL_POSTING,
+  SET_DEP_SORT,
+  SET_REFERENCE_FIXES,
+  SET_SECTOR_DATA,
+  SET_SECTOR_ID,
+  TOGGLE_SECTOR
 } from './actionTypes';
+import {SectorDataType} from "../types";
+import {polygon} from "@turf/turf";
 
 export const addAclCid = (cid: string) => ({
   type: ADD_ACL_CID,
@@ -42,3 +52,17 @@ export const setAclSort = (name: string, sector: boolean) => ({type: SET_ACL_SOR
 export const setDepSort = (name: string) => ({type: SET_DEP_SORT, payload: {name: name, sector: false}});
 export const setAclPosting = (value: boolean) => ({type: SET_ACL_MANUAL_POSTING, payload: {manualPosting: value}});
 export const setDepPosting = (value: boolean) => ({type: SET_DEP_MANUAL_POSTING, payload: {manualPosting: value}});
+
+export const setSectors = (sectors: SectorDataType[]) => ({
+  type: SET_SECTOR_DATA,
+  payload: Object.fromEntries(sectors.map((sector: SectorDataType) => [sector.properties.id, polygon(sector.geometry.coordinates, sector.properties)]))
+});
+
+export const setReferenceFixes = (referenceFixes: any[]) => ({
+  type: SET_REFERENCE_FIXES,
+  payload: referenceFixes
+});
+
+export const toggleSector = (id: string) => ({type: TOGGLE_SECTOR, payload: id});
+export const setArtccId = (id: string) => ({type: SET_ARTCC_ID, payload: id});
+export const setSectorId = (id: string) => ({type: SET_SECTOR_ID, payload: id});
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { aclReducer } from './reducers/aclReducer'
-import { depReducer } from './reducers/depReducer'
-// ...
+import {aclReducer} from './reducers/aclReducer';
+import {depReducer} from './reducers/depReducer';
+import {sectorReducer} from "./reducers/sectorReducer";
+import {configureStore} from "@reduxjs/toolkit";
+import {entriesReducer} from "./reducers/entriesReducer";
+
+const asyncFunctionMiddleware = (storeAPI: any) => (next: any) => (action: any) => {
+  // If the "action" is actually a function instead...
+  if (typeof action === 'function') {
+    // then call the function and pass `dispatch` and `getState` as arguments
+    return action(storeAPI.dispatch, storeAPI.getState);
+  }
+
+  // Otherwise, it's a normal action - send it onwards
+  return next(action);
+};
 
 const store = configureStore({
-    reducer: {
-        acl: aclReducer,
-        dep: depReducer
-    },
-})
+  reducer: {
+    acl: aclReducer,
+    dep: depReducer,
+    sectorData: sectorReducer,
+    entries: entriesReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(asyncFunctionMiddleware)
+});
 
 export default store;
 
