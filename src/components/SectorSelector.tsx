3b551d28daa06c56435ba48818b99e741eb8625c
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-03-04 18:09:02 +0100

    add button to open sector selector

--- src/components/SectorSelector.tsx
+++ src/components/SectorSelector.tsx
@@ -9,6 +9,7 @@ import {refreshEntriesThunk} from "../redux/slices/entriesSlice";
 export const SectorSelector: React.FC = () => {
   const dispatch = useAppDispatch();
   const sectors = useAppSelector((state) => state.sectorData.sectors);
+  const selectedSectors = useAppSelector((state) => state.sectorData.selectedSectors)
 
   return (
     <div className="sector-selector">
@@ -16,20 +17,19 @@ export const SectorSelector: React.FC = () => {
         rel="stylesheet"
         href={"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"}
       />
-      <div className="boundary-selector-innner">
+      <div>
         <hr style={{color: '#000000', padding: 20, border: 'none'}}/>
         <h1>Pick your sectors</h1>
         <form>
           <div className="checkbox-block">
             {Object.keys(sectors).map((id) =>
-              [<input key={`boundary-selector-${id}-input`} type="checkbox" className="checkbox-effect checkbox-effect-2"
-                      onChange={() => dispatch(toggleSector(id))} id={id} value={id} name={id}
+              [<input key={`sector-selector-${id}-input`} type="checkbox" className="checkbox-effect checkbox-effect-2"
+                      onChange={() => dispatch(toggleSector(id))} id={id} value={id} name={id} checked={selectedSectors.includes(id)}
               />,
-                <label key={`boundary-selector-${id}-label`} htmlFor={id}>{id}</label>
+                <label key={`sector-selector-${id}-label`} htmlFor={id}>{id}</label>
               ])}
           </div>
         </form>
-        {/*<button className="close-btn" onClick={() => props.changer(false)}>Close</button>*/}
         <EdstButton className="no-select" content="Save" onMouseDown={() => {
           dispatch(refreshEntriesThunk());
           dispatch(setShowSectorSelector(false));
