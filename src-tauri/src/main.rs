ec4291454dfc41441526d71de90e45c2b2cd9859
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-04-24 01:03:43 +0200

    fix cursor repositioning function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,14 +6,8 @@
 use tauri::{Position::Logical,LogicalPosition};
 
 #[tauri::command]
-fn set_cursor_position(window: tauri::Window, x: i32, y: i32) {
-  let inner_pos = window.inner_position().unwrap();
-  let inner_size = window.inner_size().unwrap();
-  let outer_size = window.outer_size().unwrap();
-  if window.set_cursor_position
-    (Logical(LogicalPosition{x: (x + inner_pos.x) as f64,
-                             y: (y + inner_pos.y + (outer_size.height as i32 - inner_size.height as i32)) as f64
-    })).is_err() {
+fn set_cursor_position(window: tauri::Window, x: f64, y: f64) {
+  if window.set_cursor_position(Logical(LogicalPosition{x: x, y: y})).is_err() {
     panic!("setting cursor failed");
   }
 }
