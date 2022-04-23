d8d1f6bbc885c364909aedd03293d88be048c082
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-04-23 10:54:11 +0200

    use tauri API to move mouse cursor

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,27 +3,18 @@
   windows_subsystem = "windows"
 )]
 
-#[cfg(target_os = "windows")]
-#[tauri::command]
-fn set_pointer_position(window: tauri::Window, x: i32, y: i32) {
-  let inner_size = window.inner_size().unwrap();
-  let outer_size = window.outer_size().unwrap();
-  let pos = window.inner_position().unwrap();
-  unsafe {
-    if winapi::um::winuser::SetPhysicalCursorPos(pos.x + x, pos.y + y + (outer_size.height as i32 - inner_size.height as i32)) == 0 {
-      panic!("SetCursorPos failed");
-    }
-  }
-}
+use tauri::{Position::Physical,PhysicalPosition};
 
-#[cfg(target_os = "linux")] // TODO: mouse_rs supposedly also works with windows. check this when able.
 #[tauri::command]
 fn set_pointer_position(window: tauri::Window, x: i32, y: i32) {
-  let inner_size = window.inner_size().unwrap();
-  let outer_size = window.outer_size().unwrap();
-  let pos = window.inner_position().unwrap();
-  let mouse = mouse_rs::Mouse::new();
-  mouse.move_to(pos.x + x, pos.y + y + (outer_size.height as i32 - inner_size.height as i32)).expect("Unable to move mouse");
+    let inner_pos = window.inner_position().unwrap();
+    let inner_size = window.inner_size().unwrap();
+    let outer_size = window.outer_size().unwrap();
+    let result = window.set_cursor_position
+        (Physical(PhysicalPosition{x: x + inner_pos.x, y: y + inner_pos.y + (outer_size.height as i32 - inner_size.height as i32)}));
+    if result.is_err() {
+        panic!("setting cursor failed");
+    }
 }
 
 fn main() {
