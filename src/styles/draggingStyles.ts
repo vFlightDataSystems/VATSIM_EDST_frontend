a5182453c90ccb739e6ba8ef8b47ca4ec27b2483
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-05-18 01:47:47 +0200

    fix my mistakes :(

--- src/styles/draggingStyles.ts
+++ src/styles/draggingStyles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const EdstDraggingOutline = styled.div`
+  pointer-events: all;
   position: fixed;
   z-index: 20000;
   background-color: transparent;
