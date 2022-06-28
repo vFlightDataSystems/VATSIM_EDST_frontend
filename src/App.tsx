673fd2ab7002260fb97c5ca6ebbc73c67041fe46
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-06-28 15:34:50 +0200

    remove public route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import EdstProvider from "./Edst";
-import PublicRoute from "./routes/PublicRoute";
 import Login from "./login/Login";
 import PrivateRoute from "./routes/PrivateRoute";
 
@@ -9,9 +8,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<PublicRoute />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<PrivateRoute />}>
           <Route path="/" element={<EdstProvider />} />
         </Route>
