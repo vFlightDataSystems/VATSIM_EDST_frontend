0218cf0584b7559415f14bf9581e6a727b3fa89b
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-08-10 17:53:58 +0200

    add ERAM font

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-export const defaultFontFamily = "Consolas, MyConsolas";
-// export const defaultFontFamily = "ERAM, Consolas, MyConsolas";
+// export const defaultFontFamily = "Consolas, MyConsolas";
+export const defaultFontFamily = "ERAM, Consolas, MyConsolas";
 export const defaultFontSize = "16px";
 export const defaultInputFontSize = "16px";
 
