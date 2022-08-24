b2e7e19c4c91d644ed15df897c5d70465bd51e55
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-08-24 19:07:12 +0200

    refactor SharedAircraftDto.ts

--- src/types/sharedStateTypes/sharedAircraftDto.ts
+++ src/types/sharedStateTypes/sharedAircraftDto.ts
@@ -1,14 +1,42 @@
-export type SharedAircraftDto = {
-  aircraftId: string;
-  spa: boolean;
-  aclDisplay: boolean;
-  aclDeleted: boolean;
-  depDisplay: boolean;
-  depDeleted: boolean;
-  vciStatus: -1 | 0 | 1;
-  depStatus: -1 | 0 | 1;
-  aclHighlighted: boolean;
-  depHighlighted: boolean;
-  freeTextContent: string;
-  showFreeText: boolean;
-};
+import { AircraftId } from "../aircraftId";
+
+export class SharedAircraftDto {
+  constructor(data: SharedAircraftDto) {
+    this.aircraftId = data.aircraftId;
+    this.spa = data.spa;
+    this.aclDisplay = data.aclDisplay;
+    this.aclDeleted = data.aclDeleted;
+    this.depDisplay = data.depDisplay;
+    this.depDeleted = data.depDeleted;
+    this.vciStatus = data.vciStatus;
+    this.depStatus = data.depStatus;
+    this.aclHighlighted = data.aclHighlighted;
+    this.depHighlighted = data.depHighlighted;
+    this.freeTextContent = data.freeTextContent;
+    this.showFreeText = data.showFreeText;
+  }
+
+  aircraftId: AircraftId = "";
+
+  spa = false;
+
+  aclDisplay = false;
+
+  aclDeleted = false;
+
+  depDisplay = false;
+
+  depDeleted = false;
+
+  vciStatus: -1 | 0 | 1 = -1;
+
+  depStatus: -1 | 0 | 1 = -1;
+
+  aclHighlighted = false;
+
+  depHighlighted = false;
+
+  freeTextContent = "";
+
+  showFreeText = false;
+}
