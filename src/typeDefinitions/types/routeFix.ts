216fc0275c17b314f46139cb1af81150d87f1e8f
Milan <34196765+CaptainTux@users.noreply.github.com>
2022-09-14 15:33:27 +0200

    refactor routeFix types

--- src/typeDefinitions/types/routeFix.ts
+++ src/typeDefinitions/types/routeFix.ts
@@ -3,8 +3,4 @@ import { Position } from "@turf/turf";
 export type RouteFix = {
   name: string;
   pos: Position;
-  // distance from present position were applicable
-  dist?: number;
-  // UTC minutes when aircraft reaches the fix
-  minutesAtFix?: number;
 };
--- src/typeDefinitions/types/routeFixWithDistance.ts
+++ src/typeDefinitions/types/routeFixWithDistance.ts
@@ -1,3 +1,8 @@
 import { RouteFix } from "./routeFix";
 
-export type RouteFixWithDistance = RouteFix & { dist: number };
+export type RouteFixWithDistance = RouteFix & {
+  // distance from present position were applicable
+  dist: number;
+  // UTC minutes when aircraft reaches the fix
+  minutesAtFix?: number;
+};
