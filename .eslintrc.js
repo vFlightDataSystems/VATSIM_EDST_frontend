b88329751a9f99fcdccc30dc68c7c9c0eb6174b2
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-09-24 06:56:02 +0200

    fix packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     "airbnb",
+    "react-app",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
