03edb63d07c4a457af13fc2ea8fdb9fb01d9a7f9
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-09-24 05:42:27 +0200

    upgrade to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import "./css/styles.scss";
@@ -13,11 +13,12 @@ declare global {
   }
 }
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
