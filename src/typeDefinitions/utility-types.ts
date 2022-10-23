8482e01e63974a89a6d0ed1f059b97ddd22f4447
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-10-23 03:31:28 +0200

    add AtMostOne utility type

--- src/typeDefinitions/utility-types.ts
+++ src/typeDefinitions/utility-types.ts
@@ -1,2 +1,3 @@
 export type Nullable<T> = T | null;
 export type AllOrNone<T> = T | { [K in keyof T]?: never };
+export type AtMostOne<T> = { [K in keyof T]: Partial<Pick<T, K>> & { [L in keyof Omit<T, K>]?: never } }[keyof T];
