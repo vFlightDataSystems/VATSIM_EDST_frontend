fb7e9e21679627cd5d156b71487c26a3adabade5
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-10-25 03:43:16 +0200

    add assert function for error handling (to be added)

--- src/utility-functions.ts
+++ src/utility-functions.ts
@@ -1,3 +1,7 @@
-export const unsafeKeys = Object.keys as <T>(o: T) => (keyof T)[];
 export const unsafeEntries = Object.entries as <T, K extends string>(o: Record<K, T>) => [K, T][];
 export const isEnum = <T extends Record<string, string>>(enumObj: T) => (s: string): s is T[keyof T] => Object.values(enumObj).includes(s);
+export function assert(value: unknown, message: string): asserts value {
+  if (!value) {
+    throw new Error(message);
+  }
+}
