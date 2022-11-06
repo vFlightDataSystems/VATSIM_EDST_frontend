2dc4c4c0a6a966929dcaff2dc5bb6edcc8a030f3
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-11-06 14:51:54 +0100

    simplify useSocketConnector.ts

--- src/hooks/useSocketConnector.ts
+++ src/hooks/useSocketConnector.ts
@@ -2,6 +2,5 @@ import { useContext } from "react";
 import { SocketContext } from "~/contexts/SocketContext";
 
 export const useSocketConnector = () => {
-  const { connectSocket, disconnectSocket, isConnected } = useContext(SocketContext);
-  return { connectSocket, disconnectSocket, isConnected };
+  return useContext(SocketContext);
 };
