70e67dc3d0ce68aa543aa324db9b952d72a98a8d
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-11-11 15:22:39 +0100

    remove wrapper

--- src/components/utils/EdstDraggingOutline.tsx
+++ src/components/utils/EdstDraggingOutline.tsx
@@ -1,22 +1,14 @@
 import styled from "styled-components";
-import React from "react";
 import type { DragPreviewStyle } from "types/dragPreviewStyle";
 
-const EdstDraggingOutlineDiv = styled.div<{ absolute?: boolean }>`
+type EdstDraggingOutlineProps = {
+  absolute?: boolean;
+  style: DragPreviewStyle;
+};
+export const EdstDraggingOutline = styled.div<EdstDraggingOutlineProps>`
   position: ${(props) => (props.absolute ? "absolute" : "fixed")};
   pointer-events: all;
   z-index: 30000;
   background-color: transparent;
   border: 1px solid #ffffff;
 `;
-
-type EdstDraggingOutlineProps = {
-  absolute?: boolean;
-  style: DragPreviewStyle;
-  onMouseUp?: () => void;
-  onMouseDown?: () => void;
-};
-
-export const EdstDraggingOutline = (props: EdstDraggingOutlineProps) => {
-  return <EdstDraggingOutlineDiv {...props} />;
-};
