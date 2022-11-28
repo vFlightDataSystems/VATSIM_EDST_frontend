43d003e4e398d8baf9a2c8700ebf439c05eec54c
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-11-28 21:53:54 +0100

    add projectTrajectory function

---
+++ src/utils/projectTrajectory.ts
@@ -0,0 +1,21 @@
+import type { RouteFix } from "types/routeFix";
+import type { ApiAircraftTrack } from "types/apiTypes/apiAircraftTrack";
+import { locationToPosition } from "~/utils/locationToPosition";
+import { getRouteLineString } from "~/utils/getRouteLineString";
+import type { Position } from "@turf/turf";
+import { along } from "@turf/turf";
+
+export function projectTrajectory(fixes: RouteFix[], track: ApiAircraftTrack) {
+  const pos = locationToPosition(track.location);
+  const routeLine = getRouteLineString(fixes, pos);
+  if (routeLine && track.groundSpeed > 0) {
+    const projectedPositions: Position[] = [];
+    // compute position for each seconds in the next 20 minutes
+    for (let i = 1; i <= 1200; i++) {
+      const position = along(routeLine, (track.groundSpeed * i) / 3600, { units: "nauticalmiles" }).geometry.coordinates;
+      projectedPositions.push(position);
+    }
+    return { time: Date.now() / 1000, projectedPositions };
+  }
+  return null;
+}
