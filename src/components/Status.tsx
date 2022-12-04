cc8c1efd52226a9e8ab6f790a6011d3af83bdfc3
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-12-04 15:39:26 +0100

    refactor Status.tsx and set version

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -16,7 +16,7 @@ export const Status = () => {
   const hubConnection = useHubConnection();
   const { connectSocket, disconnectSocket, isConnected } = useSocketConnector();
 
-  const onClickToggleSocket = () => {
+  const toggleSocket = () => {
     if (isConnected) {
       disconnectSocket();
     } else if (hubConnection?.state === "Connected" && artccId && sectorId) {
@@ -26,14 +26,10 @@ export const Status = () => {
 
   return (
     <FloatingWindow title="STATUS" optionsHeaderTitle="STATUS" width="40ch" window="STATUS" showOptions={showOptions} setShowOptions={setShowOptions}>
-      <EdstButton onMouseDown={onClickToggleSocket} content={`${isConnected ? "Disable" : "Enable"} Shared State`} />
-      <span>
-        Submit Feedback{" "}
-        <a href="https://forms.gle/LpzgyNMNMwa8CY8e8" target="_blank" rel="noreferrer">
-          here
-        </a>{" "}
-        or on the Discord server.
-      </span>
+      <p>vEDST version 0.1.0</p>
+      <p>
+        <EdstButton onMouseDown={toggleSocket} content={`${isConnected ? "Disable" : "Enable"} Shared State`} />
+      </p>
     </FloatingWindow>
   );
 };
