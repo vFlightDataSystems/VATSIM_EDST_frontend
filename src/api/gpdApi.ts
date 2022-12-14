eacfe442a0fd56ed8c1c6c6532eff2be6e32cfca
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-12-14 15:47:30 +0100

    use queryFn for gpdApi

--- src/api/gpdApi.ts
+++ src/api/gpdApi.ts
@@ -2,16 +2,23 @@
    hooks corresponding to the defined endpoints */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { FeatureCollection } from "geojson";
-import { useRootSelector } from "~redux/hooks";
-import { configSelector } from "~redux/slices/authSlice";
 
 // Define a service using a base URL and expected endpoints
 export const gpdApi = createApi({
   reducerPath: "gpdApi",
   baseQuery: fetchBaseQuery({ baseUrl: "" }),
   endpoints: (builder) => ({
-    getArtccBoundaries: builder.query<FeatureCollection, string>({
-      query: (url) => ({ url }),
+    getArtccBoundaries: builder.query<FeatureCollection, Record<never, never>>({
+      queryFn: async (_, { getState }) => {
+        // we cannot put RootState here because then gpdApi would reference itself
+        const url = (getState() as any).auth.vnasConfig.artccBoundariesUrl;
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("could not fetch ARTCC boundaries");
+        }
+        const data = await response.json();
+        return { data };
+      },
     }),
   }),
 });
@@ -19,6 +26,5 @@ export const gpdApi = createApi({
 const { useGetArtccBoundariesQuery } = gpdApi;
 
 export const useArtccBoundaries = () => {
-  const config = useRootSelector(configSelector)!;
-  return useGetArtccBoundariesQuery(config.artccBoundariesUrl);
+  return useGetArtccBoundariesQuery({});
 };
