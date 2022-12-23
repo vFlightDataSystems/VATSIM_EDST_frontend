eb4474f70cb7da360352bf1cc9c5a204033b9cc7
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-12-23 15:08:17 +0100

    fix vite.config.ts and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import autoprefixer from "autoprefixer";
-import loadVersion from "vite-plugin-package-version";
 // import * as path from "path";
 
 // https://vitejs.dev/config/
@@ -23,7 +22,7 @@ export default defineConfig({
       plugins: [autoprefixer({})],
     },
   },
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), loadVersion()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   // resolve: {
   //   alias: {
   //     "react-dom/client": path.resolve("./node_modules/react-dom/profiling"),
