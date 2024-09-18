f2748ac881bb74e907ae03a8b1420a60072a1c40
MatthewArmstrong1 <matthewarmstrong862@gmail.com>
2024-09-18 20:30:20 +0100

    remove unneeded package and cleanup routing file

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,15 +1,12 @@
-import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { vatsimTokenSelector, sessionActiveSelector } from "~redux/slices/authSlice";
 import { useRootSelector } from "~redux/hooks";
 
 const PrivateRoute = () => {
   const token = useRootSelector(vatsimTokenSelector);
-  const sessionActive = useRootSelector(sessionActiveSelector);
 
-  //localStorage.removeItem("vatsim-token");
+  // User needs a valid token and a connection before viewing EDST
 
-  // User needs a valid token and an active session before viewing EDST
   return (token) ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
